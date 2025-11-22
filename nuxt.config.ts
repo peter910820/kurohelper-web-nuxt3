632beb07a7a16b9b207edf2406a891797fd5fb4c
SeaotterMS <peter910820@gmail.com>
2025-11-22 16:25:54 +0800

    chore: change css framework to vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  modules: ["@vuestic/nuxt"],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    plugins: [vuetify({ autoImport: true })],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
